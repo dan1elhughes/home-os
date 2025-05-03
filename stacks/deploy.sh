e38e952432acaed19cae935ac6ff19caf8f61d3b
Dan Hughes <github@danhughes.dev>
2025-05-03 10:54:54 +0100

    Deploy single stack arg

--- stacks/deploy.sh
+++ stacks/deploy.sh
@@ -1,14 +1,21 @@
 export $(cat .env)
 
-# For each directory, deploy the stack
-
-for dir in $(ls -d */); do
-    # Check if the directory contains a docker-compose.yml file
+deploy_stack() {
+    local dir="$1"
     if [ -f "$dir/docker-compose.yml" ]; then
         echo "Deploying stack for $dir"
-        # Deploy the stack using docker stack deploy
         docker stack deploy --compose-file "$dir/docker-compose.yml" "${dir%/}" --detach=true
     else
         echo "No docker-compose.yml found in $dir, skipping."
     fi
-done
+}
+
+# Check if a CLI argument is passed
+if [ "$1" ]; then
+    deploy_stack "$1"
+else
+    # For each directory, deploy the stack
+    for dir in $(ls -d */); do
+        deploy_stack "$dir"
+    done
+fi
