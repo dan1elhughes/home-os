19469caf34688735639b079ef671c82b0e1ea0e6
Dan Hughes <github@danhughes.dev>
2025-07-22 18:52:51 +0100

    Remove portainer_agent from exclude list in rebalance script

--- stacks/rebalance.sh
+++ stacks/rebalance.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-EXCLUDE_LIST="(portainer_agent|NAME)"
+EXCLUDE_LIST="(NAME)"
 
 for service in $(docker service ls | egrep -v $EXCLUDE_LIST | awk '{print $2}'); do
     docker service update --force $service
