9eb905997e38946227d43a11bfb93d16f6587556
Dan Hughes <github@danhughes.dev>
2026-02-26 23:29:34 +0000

    Add --with-registry-auth to docker stack deploy in deploy.sh

--- stacks/deploy.sh
+++ stacks/deploy.sh
@@ -6,7 +6,7 @@ deploy_stack() {
     local dir="$1"
     if [ -f "$dir/docker-compose.yml" ]; then
         echo "Deploying stack for $dir"
-        docker stack deploy --compose-file "$dir/docker-compose.yml" "${dir%/}" --detach=true
+        docker stack deploy --compose-file "$dir/docker-compose.yml" "${dir%/}" --detach=true --with-registry-auth
     else
         echo "No docker-compose.yml found in $dir, skipping."
     fi
